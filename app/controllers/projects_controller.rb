cffd40773d2e4cabe2b23bf07771b91dac04922a
MKanko <mkanko66@gmail.com>
2019-07-04 14:40:31 -0700

    add get projects (index) action and get projects new (create) action to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,20 @@
 class ProjectsController < ApplicationController
 
+    get '/projects' do                           # index
+        if logged_in?                            # page should show all user projects with link to
+            @projects = current_user.projects    # edit a project - '/projects/:id/edit'  
+            erb :'projects/index'                # or delete a project - '/projects/:id/delete'
+        else                                     # should have link to create a new project - '/projects/new'
+            redirect to '/login'
+        end 
+    end
+
+    get '/projects/new' do                     # form to create new project
+        if logged_in?
+            erb :'projects/new' 
+        else 
+            redirect to '/login'
+        end 
+    end
+
 end 
