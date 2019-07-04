21a98d647456b42dde0b96b622247424a2679fa7
MKanko <mkanko66@gmail.com>
2019-07-04 14:11:26 -0700

    add signup controller action to users controller and signup form to users/signup.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,11 @@
 class UsersController < ApplicationController
+
+    get '/signup' do               # sign up form for new user
+        if !logged_in? 
+            erb :'users/signup'
+        else
+            redirect to '/projects'
+        end 
+    end
     
 end 
